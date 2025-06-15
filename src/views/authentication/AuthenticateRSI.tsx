db224050e2170448156dc6d98d06bd2b707f5996
henry232323 <14969260+henry232323@users.noreply.github.com>
2025-06-15 13:21:16 -0700

    Redirect after verification

--- src/views/authentication/AuthenticateRSI.tsx
+++ src/views/authentication/AuthenticateRSI.tsx
@@ -89,6 +89,7 @@ export function AuthenticateRSI() {
             severity: "success",
           })
           setUsername("")
+          navigate("/")
         })
         .catch((error) => {
           issueAlert(error)
