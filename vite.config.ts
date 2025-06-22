183929eeb1fbbca1f7a1e2b58cd470dc227303fc
henry232323 <14969260+henry232323@users.noreply.github.com>
2025-06-22 11:03:09 -0700

    Update type dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,7 @@ import svgrPlugin from "vite-plugin-svgr"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      jsxRuntime: "classic",
-    }),
+    react(),
     viteTsconfigPaths(),
     svgrPlugin(),
   ],
